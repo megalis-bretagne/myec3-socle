a72c0aaf5e5f37f0c3fc13647ade06b493e72232
louetcpresta <christopher.louet.presta@sib.fr>
2020-03-11 18:10:09 +0100

    MyEC3/EC56 Mise à jour config glowroot webapp.

--- myec3-socle-webapp-module/myec3-socle-webapp/docker-entrypoint.sh
+++ myec3-socle-webapp-module/myec3-socle-webapp/docker-entrypoint.sh
@@ -4,7 +4,7 @@ ENV NODE_HOSTNAME
 export NODE_HOSTNAME=$(echo "$NODE_HOSTNAME"|sed 's/\"//g'|sed 's/ //g')
 echo "NODE_HOSTNAME: $NODE_HOSTNAME"
 
-JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.id=megalis::webapp::${NODE_HOSTNAME}::${HOSTNAME} -javaagent:/glowroot/glowroot.jar"
+JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.id=megalis::webapp::${NODE_HOSTNAME} -javaagent:/glowroot/glowroot.jar"
 echo "JAVA_OPTS: ${JAVA_OPTS}"
 
 BDD_PASSWORD=$(cat /run/secrets/db_password)
