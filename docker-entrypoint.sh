0a575d8424f4651981fd76b85650ac6f1301e8c6
louetcpresta <christopher.louet.presta@sib.fr>
2020-03-11 18:23:20 +0100

    MyEC3/EC56 Mise à jour config glowroot webapp.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ ENV NODE_HOSTNAME
 export NODE_HOSTNAME=$(echo "$NODE_HOSTNAME"|sed 's/\"//g'|sed 's/ //g')
 echo "NODE_HOSTNAME: $NODE_HOSTNAME"
 
-JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.id=megalis::esb::${NODE_HOSTNAME} -javaagent:/glowroot/glowroot.jar"
+export JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.id=megalis::esb::${NODE_HOSTNAME} -javaagent:/glowroot/glowroot.jar"
 echo "JAVA_OPTS: ${JAVA_OPTS}"
 
 catalina.sh run
