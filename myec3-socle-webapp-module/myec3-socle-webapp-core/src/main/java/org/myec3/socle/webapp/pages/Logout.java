4230c82ad806055a6c1b5c7bfa7d58000fd83720
thibaultj <joseph.thibault@sib.fr>
2020-03-20 14:18:04 +0100

    EC-47 - Se passer d'apache pour le socle - 1J

--- myec3-socle-webapp-module/myec3-socle-webapp-core/src/main/java/org/myec3/socle/webapp/pages/Logout.java
+++ myec3-socle-webapp-module/myec3-socle-webapp-core/src/main/java/org/myec3/socle/webapp/pages/Logout.java
@@ -18,13 +18,17 @@
 package org.myec3.socle.webapp.pages;
 
 import org.apache.tapestry5.EventConstants;
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Cookies;
 import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.RequestGlobals;
 import org.myec3.socle.webapp.constants.GuWebAppConstants;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -43,16 +47,27 @@ import java.net.URL;
  */
 public class Logout extends AbstractPage {
 
+//	@Inject
+//	private Cookies cookies;
+
+	@InjectPage
+	private Index index;
+
 	@Inject
-	private Cookies cookies;
+	private RequestGlobals requestGlobals;
 
 	@OnEvent(EventConstants.ACTIVATE)
-	public URL onActivate() {
+	public Index onActivate() {
 		//TODO solution temporaire en attendant de voir comment on s'intègre vraiment avec le portail
 		try{
-			cookies.removeCookieValue("mod_auth_openidc_session");
-			return new URL(GuWebAppConstants.KEYCLOAK_BASE_URL + "/auth/realms/megalis/protocol/openid-connect/logout?redirect_uri=" + GuWebAppConstants.MYEC3_BASE_URL);
-		} catch (IOException e) {
+			requestGlobals.getHTTPServletRequest().logout();
+			return index;
+//			cookies.removeCookieValue("mod_auth_openidc_session");
+//			return new URL(GuWebAppConstants.KEYCLOAK_BASE_URL + "/auth/realms/megalis/protocol/openid-connect/logout?redirect_uri=" + GuWebAppConstants.MYEC3_BASE_URL);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return null;
+		} catch (ServletException e) {
 			e.printStackTrace();
 			return null;
 		}
