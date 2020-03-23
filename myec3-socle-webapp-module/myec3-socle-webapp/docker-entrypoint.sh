14695ef83f1a9ed114e681c0ca2e85bd2df2051e
guenneuguesy <yann.guenneugues@sib.fr>
2020-03-23 10:17:11 +0100

    suppression language=fr

--- myec3-socle-webapp-module/myec3-socle-webapp/docker-entrypoint.sh
+++ myec3-socle-webapp-module/myec3-socle-webapp/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 export NODE_HOSTNAME=$(echo "$NODE_HOSTNAME"|sed 's/\"//g'|sed 's/ //g')
 echo "NODE_HOSTNAME: $NODE_HOSTNAME"
 
-export JAVA_OPTS="$JAVA_OPTS ${PROXY_JAVA_OPTS} -Duser.language=fr -Duser.country=FR -Duser.timezone=Europe/Paris -Dglowroot.agent.id=megalis::webapp::${NODE_HOSTNAME} -javaagent:/glowroot/glowroot.jar"
+export JAVA_OPTS="$JAVA_OPTS ${PROXY_JAVA_OPTS} -Duser.timezone=Europe/Paris -Dglowroot.agent.id=megalis::webapp::${NODE_HOSTNAME} -javaagent:/glowroot/glowroot.jar"
 echo "JAVA_OPTS: ${JAVA_OPTS}"
 
 BDD_PASSWORD=$(cat /run/secrets/db_password)
