5ce15b65b72a7b4ec06d0fcf13e3fa332c5a7895
thibaultj <joseph.thibault@sib.fr>
2020-04-23 14:58:37 +0200

    EC-72 - Gestion des logos des organismes

--- myec3-socle-webapp-module/myec3-socle-webapp-core/src/main/java/org/myec3/socle/webapp/services/AppModule.java
+++ myec3-socle-webapp-module/myec3-socle-webapp-core/src/main/java/org/myec3/socle/webapp/services/AppModule.java
@@ -90,7 +90,7 @@ public class AppModule {
 		configuration.add("/oauth/.*");
 		configuration.add("/sdmInit/.*");
 		configuration.add("/health");
-		configuration.add("/logos");
+		configuration.add("/logos/.*");
 	}
 
 	public HttpServletRequestFilter buildUtf8Filter() {
