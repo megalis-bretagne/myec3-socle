920147c743f815a8c602bf46c973b81d1d55f383
guenneuguesy <yann.guenneugues@sib.fr>
2020-10-23 13:46:13 +0200

    Correction parametre JVM pour docker

--- myec3-socle-esb/docker-entrypoint.sh
+++ myec3-socle-esb/docker-entrypoint.sh
@@ -4,7 +4,7 @@ export NODE_HOSTNAME=$(echo "$NODE_HOSTNAME"|sed 's/\"//g'|sed 's/ //g')
 echo "NODE_HOSTNAME: $NODE_HOSTNAME"
 
 #Parametre gestion de mémoire par la limitation de ram donnée par docker (spécifique java8)
-JAVA_OPTS="$JAVA_OPTS_ADD -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
+JAVA_OPTS="$JAVA_OPTS_ADD -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0"
 #glowroot
 JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.id=megalis::esb::${NODE_HOSTNAME} -javaagent:/glowroot/glowroot.jar"
 
