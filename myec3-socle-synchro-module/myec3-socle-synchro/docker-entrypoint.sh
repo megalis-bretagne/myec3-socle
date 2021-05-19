cb70b2c854f1e456664a02d5f75baea698074b0c
JOLIVET Sylvain <sylvain.jolivet.presta@sib.fr>
2021-05-19 17:29:06 +0200

    [FEAT] MEGALIS-185 : ajout script sql pour supprimer les triggers de Quartz qui peuvent bloquer le démarrage

--- myec3-socle-synchro-module/myec3-socle-synchro/docker-entrypoint.sh
+++ myec3-socle-synchro-module/myec3-socle-synchro/docker-entrypoint.sh
@@ -14,7 +14,21 @@ echo "JAVA_OPTS: ${JAVA_OPTS}"
 BDD_PASSWORD=$(cat /run/secrets/db_password)
 echo "bdd.pwd = $BDD_PASSWORD" > /configGen/pwd.properties
 
+
 export JPDA_ADDRESS=8000
 export JPDA_TRANSPORT=dt_socket
+
+#CLEAN TRIGGERS QUARTZ on startup
+BDD_HOST=$(cat /config/db.properties | grep bdd.host | sed 's/bdd.host=//g')
+BDD_USER=$(cat /config/db.properties | grep bdd.user | sed 's/bdd.user=//g')
+BDD_PORT=$(cat /config/db.properties | grep bdd.port | sed 's/bdd.port=//g')
+DB_SYNCHRO=$(cat /config/db.properties | grep bdd.synchro.schema | sed 's/bdd.synchro.schema=//g')
+
+echo "Clean QRTZ BEFORE Starting"
+echo "mysql --host=$BDD_HOST --port=$BDD_PORT -u $BDD_USER -D $DB_SYNCHRO"
+
+CLEAN_QRTZ=$(mysql --host=$BDD_HOST --port=$BDD_PORT -u $BDD_USER --password=$BDD_PASSWORD -D $DB_SYNCHRO -e "DELETE FROM QRTZ_PARALLEL_TRIGGERS WHERE TRIGGER_NAME NOT IN (SELECT TRIGGER_NAME FROM QRTZ_PARALLEL_SIMPLE_TRIGGERS) AND SCHED_NAME = 'parallelScheduler'")
+
+echo $CLEAN_QRTZ
 
 catalina.sh run
