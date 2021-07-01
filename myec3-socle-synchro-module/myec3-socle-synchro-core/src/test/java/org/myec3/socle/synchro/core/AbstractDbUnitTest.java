e3f56321ed558d6f5deb2198f4cb45fe9fa0ecdf
JOLIVET Sylvain <sylvain.jolivet.presta@sib.fr>
2021-07-01 11:48:44 +0200

    [TU] Ajout TU sur la partie synchro core

--- myec3-socle-synchro-module/myec3-socle-synchro-core/src/test/java/org/myec3/socle/synchro/core/AbstractDbUnitTest.java
+++ myec3-socle-synchro-module/myec3-socle-synchro-core/src/test/java/org/myec3/socle/synchro/core/AbstractDbUnitTest.java
@@ -3,6 +3,7 @@ package org.myec3.socle.synchro.core;
 
 import org.junit.runner.RunWith;
 import org.myec3.socle.synchro.core.config.SynchroCoreConfig;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -12,5 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SynchroCoreConfig.class, loader = AnnotationConfigContextLoader.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public abstract class AbstractDbUnitTest {
 }
