69a185fc629ab7e8ee584c4549c040f090a4c31d
Emanuel Guével <emanuel.guevel@sib.fr>
2023-04-21 15:55:46 +0200

    [REFACTO] Export des agents : déduplication (ajout d'un service pour l'export CSV et extraction d'un composant pour lister les champs exportés)

---
+++ myec3-socle-webapp-module/myec3-socle-webapp-core/src/main/java/org/myec3/socle/webapp/components/ExportedFieldsTable.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2011 Atos Bourgogne
+ * 
+ * This file is part of MyEc3.
+ * 
+ * MyEc3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as published by
+ * the Free Software Foundation.
+ * 
+ * MyEc3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MyEc3. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.myec3.socle.webapp.components;
+
+/**
+ * Component class used to display a table listings the columns present in the agent export CSV file.<br>
+ * 
+ * Corresponding tapestry template file is :
+ * src/main/resources/org/myec3/socle/webapp/components/ExportedFieldsTable.tml
+ */
+public class ExportedFieldsTable {
+}
